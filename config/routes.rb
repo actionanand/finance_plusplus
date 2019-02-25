ae3c186927e9456a98949adcb011680aa031f58a
Anand Raja <meetanandns@gmail.com>
2019-02-25 17:30:48 +0000

    user profile added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+                        :registrations => 'user/registrations',
+  }
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  resources :user_stocks, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

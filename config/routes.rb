11755de7d059f87ce58c68a9dc3bb1ea78596551
Anand Raja <meetanandns@gmail.com>
2019-02-25 18:45:59 +0000

    frienship page added

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

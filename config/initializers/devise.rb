fc1ec7051d09310364d7f1ee323687bb8e6dae31
Anand Raja <meetanandns@gmail.com>
2019-03-02 11:12:47 +0000

    home page is designed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   #config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.mailer_sender = 'do_not_reply@ar.com'
+  config.mailer_sender = 'do-not-reply@ar.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

9f92438bd27365a7362bfacbb668011f00d792e0
Anand Raja <meetanandns@gmail.com>
2019-03-03 13:52:24 +0000

    s3 bucket

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,15 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  
+  private
+  
+  def picture_size
+    if picture.size > 5.megabyte
+      errors(:picture, "should be less than 5MB")
+    end
+  end
+  
 end

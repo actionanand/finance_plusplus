a28fde689438ac3766ed25b9394a6ff08a1fe39f
Anand Raja <meetanandns@gmail.com>
2019-03-03 14:14:29 +0000

    aws change

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,7 +5,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [300,300]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
